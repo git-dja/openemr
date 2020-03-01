6229749b63e32cbfa71dcf906f14934dd65198b1
David <60067850+git-dja@users.noreply.github.com>
2020-03-01 16:20:43 -0600

    addded ]2020-03-01[ RVU Production Feed
    Added daily RVU production feed at provider/carrier level (with groupings)

--- sql/sp_data-models_wip.sql
+++ sql/sp_data-models_wip.sql
@@ -7,19 +7,38 @@
 -- create ]todo[ ml et nlp library integration
 -- create ]todo[ dm feed for daily cash flow forecasting
 -- create ]todo[ dm feed for monthly ar assessment
--- create ]todo[ dm feed for monthly rvu productivity
--- created ]todo[ dm feed for monthly metric summary trending
 -- create ]todo[ dm feed for daily operational prioritization
 -- create ]todo[ dm feed for daily operational productivity
--- created ]todo[ quick list of frequent use tables
+-- created ]2020-02-02[ quick list of frequent use tables
+-- created ]2020-02-02[ dm feed for monthly metric summary trending
+-- created ]2020-03-01[ dm feed for monthly rvu productivity
 -----------------------------------------------------------
+
+
+-----------------------------------------------------------
+-- DM Feed for Monthly RVU Productivity
+-- created ]2020-03-01[ initial script
+-- scale ]3pro=#s; 50pro=#s; 900pro=#s[ expected runtimes
+-----------------------------------------------------------
+DROP PROCEDURE IF EXISTS 'sp_dm_rvuproductivity'
+CREATE STORED PROCEDRUE 'sp_dm_rvuproductivity'
+    @date-end       datetime    = null
+    @range-months   int         = null
+AS
+SELECT l.date-post, l.date-service
+        , l.id-provider, l.id-group-provider, l.id-group-department
+        , l.id-carirer, l.id-group-carrier
+        , l.amount-rvu-work, l.amount-rvu-total, l.amount-charges
+FROM 'lilith' l
+WHERE l.date-post <= ISNULL(@date-end, getdate())
+    AND l.date-post >= DATEADD(month,-1*ISNULL(@range-months,1),ISNULL(@date-end,getdate())
 
 -----------------------------------------------------------
 -- DM Feed for Metric Summary Trending
 -- created ]2020-02-02[ initial script
 -- scale ]3pro=1s; 50pro=3s; 900pro=5s[ expected runtimes
--- add ]todo[ void, payment, and ar activity
 -- add ]todo[ provider, category, and other groupings
+-- added ]2020-03-01[ void, payment, and ar activity
 -----------------------------------------------------------
 DROP PROCEDURE IF EXISTS 'sp_dm_metricsummary'
 CREATE STORED PROCEDURE 'sp_dm_metricsummary'
@@ -27,15 +46,15 @@ CREATE STORED PROCEDURE 'sp_dm_metricsummary'
     @range-months   int         = null
 AS
 SELECT
-  l.date-yyyymm, l.amount-charge, l.amount-adjust-contract, l.amount-payment, l.amount-adjust-refund, l.amount-ar-open
+  l.date-post, l.amount-charge, l.amount-adjust-contract, l.amount-payment, l.amount-adjust-refund, l.amount-ar-open
   
 FROM 'lilith' l
     LEFT JOIN 'transactions'    as t    on l.id_transaction = t.id
     LEFT JOIN 'payments'        as p    on l.id_payment = p.id
     LEFT JOIN 'voids'           as v    on l.id_void = v.id
     
-WHERE l.date-yyyymm <= ISNULL(@date-end, getdate())
-    AND l.date-yyyymm >= DATEADD(month,-1*ISNULL(@range-months,36),ISNULL(@date-end,getdate())
+WHERE l.date-post <= ISNULL(@date-end, getdate())
+    AND l.date-post >= DATEADD(month,-1*ISNULL(@range-months,36),ISNULL(@date-end,getdate())
 
 
 -----------------------------------------------------------
